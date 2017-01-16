08217562f09628f5ac9b482c25efec756f265680
EuanEdgar <euan@spicerack.co.uk>
2017-01-16 16:48:52 +0000

    Basic scrolling functionality
    Added scrollbar to post text

--- post.php
+++ post.php
@@ -9,16 +9,18 @@
 </head>
 <body onload="addLineBreaks();">
 	<div class="altheader">
-			<h1 class="altheader">TITLE TEXT</h1>
-			<h3 class="altheader">WEBSITE TAGLINE</h3>
-		</div><!-- End of mainheader -->
+		<h1 class="altheader">TITLE TEXT</h1>
+		<h3 class="altheader">WEBSITE TAGLINE</h3>
+	</div><!-- End of mainheader -->
 	<?php include'Assets/Scripts/linksBar.php' ?>
 	<div width="100%" style="width:100%; height:80%; background-color:#e0e0e0; text-align: center; margin-top:2%; border: 1px solid #050505;">
-		<table cellpadding="0" cellspacing="0" align="center" width="100%">
+		<table cellpadding="0" cellspacing="0" width="100%">
 			<tr>
 				<td width="10%"></td><!--Left spacer-->
-				<td width="70%" style="background-color:blue; text-align: left; padding-left: 30px; padding-right:10px; height: 1000px; overflow-y: scroll">
-					<?php include'Assets/Scripts/getPost.php' ?>
+				<td width="70%" style="background-color:blue; text-align: left; padding-left: 30px; padding-right:10px; overflow-y: scroll">
+					<div style="height:75vh; overflow-y: scroll;">
+						<?php include'Assets/Scripts/getPost.php' ?>
+					</div>
 				</td><!--Main post content-->
 				<td width="20%"></td><!--Right spacer-->
 			</tr>
