932c5149dfdf5703c4ad00ac746820661c4558a9
EuanEdgar <euan@spicerack.co.uk>
2017-01-16 15:34:36 +0000

    Moved linksbar CSS to appropriate file
    Removed style code from links bar.php, placed it inn links bar.css

--- Assets/Scripts/linksbar.php
+++ Assets/Scripts/linksbar.php
@@ -1,40 +1,5 @@
 <?php
-echo	"<style>
-			div.link{
-	size:100%;
-}
-
-div.linksbar{
-	width: 100%;
-	background-color: #101010;
-	border: 1px solid #050505;
-	color: #ffffff
-}
-
-table.linksbar{
-	width:100%;
-}
-td.link{
-	width:10%;
-	text-align: center;
-	border-left: 1px solid #ff9900;
-	border-right: 1px solid #ff9900;
-}
-td.link-spacer-1{
-	width:33%;
-}
-td.link-spacer-2{
-	width:2%;
-}
-
-a.link-bar{
-	color: #ffffff;
-	font-size: 15pt;
-	text-decoration: none;
-	width:100%;
-	height:100%;
-}
-		</style>
+echo	"<link rel=\"stylesheet\" type=\"text/css\" href=\"Assets/linksbar.css\">
 		<div class=\"linksbar\">
 			<table cellspacing=\"0\" cellpadding=\"0\" class=\"linksbar\">
 				<tr>
