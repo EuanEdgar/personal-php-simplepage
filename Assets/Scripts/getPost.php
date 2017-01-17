afb64ea87a14a9d635113c576c23c1c0831a62eb
EuanEdgar <euan@spicerack.co.uk>
2017-01-17 15:46:00 +0000

    Modified link structure at start of posts
    Added a link to the all categories page to the start of posts

--- Assets/Scripts/getPost.php
+++ Assets/Scripts/getPost.php
@@ -21,7 +21,7 @@
 		}
 	}
 
-	echo "<h4><a href=\"http://localhost:8888/simplepage/home.php\" class=\"content-links\">>Home</a> <a href=\"http://localhost:8888/simplepage/categories.php?category=".$category."\" class=\"content-links\">>".$category."</a>
+	echo "<h4><a href=\"http://localhost:8888/simplepage/home.php\" class=\"content-links\">> Home</a> <a href=\"http://localhost:8888/simplepage/categories.php\" class=\"content-links\">> Categories</a> <a href=\"http://localhost:8888/simplepage/categories.php?category=".$category."\" class=\"content-links\">> ".$category."</a>
 	<h2>".$title."</h2>";
 	if(strlen($imageRef)>0)echo "<img class=\"large\" src=\"".$imageRef."\">";
 	echo "<p class=\"post-text\">".$text."</p>";
