9c6c6c5928f2a199913946c165e99ad1244107e6
EuanEdgar <euan@spicerack.co.uk>
2017-01-17 10:20:18 +0000

    Unified design across post and home
    Unified the design style across home.php and post.php and began
    preparations for categories.php

--- Assets/Scripts/getPost.php
+++ Assets/Scripts/getPost.php
@@ -3,23 +3,26 @@
 	$conn;
 	include'connectToDatabase.php';
 
-	$sql = "SELECT Title, Text, ImagePath FROM simplepage.Post WHERE ID=".$id;
+	$sql = "SELECT Title, Text, ImagePath, Category FROM simplepage.Post WHERE ID=".$id;
 
 	$result = $conn->query($sql);
 
 	$title;
 	$imageRef;
 	$text;
+	$category;
 
 	if($result->num_rows > 0){
 		while($row = $result->fetch_assoc()){
 				$title = $row["Title"];
 				$imageRef = $row["ImagePath"];
 				$text = $row["Text"];
+				$category = $row["Category"];
 		}
 	}
 
-	echo "<h2>".$title."</h2>
+	echo "<a href=\"localhost:8888/simplepage/categories?cat=".$category."\"><h4>".$category."</h4></a>
+	<h2>".$title."</h2>
 	<img class=\"large\" src=\"".$imageRef."\">
 	<p class=\"post-text\">".$text."</p>"
 ?>
